c67787762ae7a0b7a61c624d23b0404944458364
zacharylangley <zacharylangley@my.unt.edu>
2018-01-10 13:15:24 -0600

    Before Firebase

--- src/app/allscores/allscores.component.ts
+++ src/app/allscores/allscores.component.ts
@@ -15,6 +15,21 @@ export class AllscoresComponent implements OnInit {
 
   ngOnInit() {
     this.competitors = this._competitorService.getCompetitors();
+    this.topSort();
+  }
+
+  private topSort() {
+    this.competitors = this.competitors.sort((n1, n2) => {
+      if (n1.score > n2.score) {
+        return 1;
+      }
+      if (n1.score < n2.score) {
+        return -1;
+      }
+
+      return 0;
+    });
+    this.competitors.reverse();
   }
 
 }
