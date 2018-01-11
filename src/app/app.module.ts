f2c376feda616a847cc5f2659fa41cf05203a43b
zacharylangley <zacharylangley@my.unt.edu>
2018-01-11 10:46:18 -0600

    FontAwesome + Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,13 +14,12 @@ import { ResourcesComponent } from './resources/resources.component';
 import { AllscoresComponent } from './allscores/allscores.component';
 import { TopComponent } from './top/top.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { CompetitorService } from './services/competitors.service';
 import { ResourceService } from './services/resources.service';
 import { appRoutes } from './app.routes';
 import { ResourceListComponent } from './resource-list/resource-list.component';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireModule } from 'angularfire2';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBdRV8q9CQ3vDJQ0VBvxhjKPZPUz9CYMNc',
@@ -37,7 +39,8 @@ export const firebaseConfig = {
     AllscoresComponent,
     TopComponent,
     NavComponent,
-    ResourceListComponent
+    ResourceListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
