0085748fbbba0a05256511022929f005835f2f6e
zacharylangley <zacharylangley@my.unt.edu>
2018-01-22 22:58:23 -0600

    Fuck it

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: false,
-
+  firebaseConfig: {
+    apiKey: 'AIzaSyBdRV8q9CQ3vDJQ0VBvxhjKPZPUz9CYMNc',
+    authDomain: 'highscoresproject.firebaseapp.com',
+    databaseURL: 'https://highscoresproject.firebaseio.com',
+    projectId: 'highscoresproject',
+    storageBucket: 'highscoresproject.appspot.com',
+    messagingSenderId: '110548734177',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,12 @@
 
 export const environment = {
   production: false,
-
+  firebaseConfig: {
+    apiKey: 'AIzaSyBdRV8q9CQ3vDJQ0VBvxhjKPZPUz9CYMNc',
+    authDomain: 'highscoresproject.firebaseapp.com',
+    databaseURL: 'https://highscoresproject.firebaseio.com',
+    projectId: 'highscoresproject',
+    storageBucket: 'highscoresproject.appspot.com',
+    messagingSenderId: '110548734177',
+  }
 };
